e82fbcc4f31c8f65bfdc2c780aa0b238704e3981
george <a7587463@gmail.com>
2020-11-07 02:09:47 +0800

    加入專案檔案。

---
+++ Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace midtterm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnpump_h = new System.Windows.Forms.Button();
+            this.MON = new System.Windows.Forms.Label();
+            this.btnUP = new System.Windows.Forms.Button();
+            this.labLog = new System.Windows.Forms.Label();
+            this.rtbLog = new System.Windows.Forms.RichTextBox();
+            this.picResult = new System.Windows.Forms.PictureBox();
+            this.btnpump_l = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picResult)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnpump_h
+            // 
+            this.btnpump_h.Font = new System.Drawing.Font("新細明體", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnpump_h.Location = new System.Drawing.Point(643, 16);
+            this.btnpump_h.Name = "btnpump_h";
+            this.btnpump_h.Size = new System.Drawing.Size(129, 125);
+            this.btnpump_h.TabIndex = 13;
+            this.btnpump_h.Text = "高階卡片";
+            this.btnpump_h.UseVisualStyleBackColor = true;
+            this.btnpump_h.Click += new System.EventHandler(this.btnpump_h_Click);
+            // 
+            // MON
+            // 
+            this.MON.AutoSize = true;
+            this.MON.Font = new System.Drawing.Font("新細明體", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.MON.Location = new System.Drawing.Point(229, 37);
+            this.MON.Name = "MON";
+            this.MON.Size = new System.Drawing.Size(58, 64);
+            this.MON.TabIndex = 12;
+            this.MON.Text = "0";
+            // 
+            // btnUP
+            // 
+            this.btnUP.Font = new System.Drawing.Font("新細明體", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnUP.Location = new System.Drawing.Point(339, 16);
+            this.btnUP.Name = "btnUP";
+            this.btnUP.Size = new System.Drawing.Size(146, 125);
+            this.btnUP.TabIndex = 11;
+            this.btnUP.Text = "錢";
+            this.btnUP.UseVisualStyleBackColor = true;
+            this.btnUP.Click += new System.EventHandler(this.btnUP_Click);
+            // 
+            // labLog
+            // 
+            this.labLog.AutoSize = true;
+            this.labLog.Font = new System.Drawing.Font("新細明體", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.labLog.Location = new System.Drawing.Point(67, 37);
+            this.labLog.Name = "labLog";
+            this.labLog.Size = new System.Drawing.Size(156, 64);
+            this.labLog.TabIndex = 10;
+            this.labLog.Text = "金幣";
+            // 
+            // rtbLog
+            // 
+            this.rtbLog.Font = new System.Drawing.Font("新細明體", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.rtbLog.Location = new System.Drawing.Point(53, 178);
+            this.rtbLog.Name = "rtbLog";
+            this.rtbLog.Size = new System.Drawing.Size(263, 357);
+            this.rtbLog.TabIndex = 9;
+            this.rtbLog.Text = "1\n2\n3\n";
+            // 
+            // picResult
+            // 
+            this.picResult.Location = new System.Drawing.Point(402, 178);
+            this.picResult.Name = "picResult";
+            this.picResult.Size = new System.Drawing.Size(322, 357);
+            this.picResult.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picResult.TabIndex = 8;
+            this.picResult.TabStop = false;
+            // 
+            // btnpump_l
+            // 
+            this.btnpump_l.Font = new System.Drawing.Font("新細明體", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnpump_l.Location = new System.Drawing.Point(491, 16);
+            this.btnpump_l.Name = "btnpump_l";
+            this.btnpump_l.Size = new System.Drawing.Size(146, 126);
+            this.btnpump_l.TabIndex = 7;
+            this.btnpump_l.Text = "低階卡片";
+            this.btnpump_l.UseVisualStyleBackColor = true;
+            this.btnpump_l.Click += new System.EventHandler(this.btnpump_l_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(914, 590);
+            this.Controls.Add(this.btnpump_h);
+            this.Controls.Add(this.MON);
+            this.Controls.Add(this.btnUP);
+            this.Controls.Add(this.labLog);
+            this.Controls.Add(this.rtbLog);
+            this.Controls.Add(this.picResult);
+            this.Controls.Add(this.btnpump_l);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picResult)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnpump_h;
+        private System.Windows.Forms.Label MON;
+        private System.Windows.Forms.Button btnUP;
+        private System.Windows.Forms.Label labLog;
+        private System.Windows.Forms.RichTextBox rtbLog;
+        private System.Windows.Forms.PictureBox picResult;
+        private System.Windows.Forms.Button btnpump_l;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,100 @@
+﻿using midtterm.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace midtterm
+{
+
+
+    public partial class Form1 : Form
+    {
+        List<Image> list = new List<Image>();
+        int m;
+        string m_str;
+        int m_l = 2, m_h = 5;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            list.Add(Resources._001);
+            list.Add(Resources._002);
+            list.Add(Resources._003);
+            list.Add(Resources._004);
+            list.Add(Resources._005);
+            list.Add(Resources._006);
+            list.Add(Resources._007);
+            list.Add(Resources._008);
+            list.Add(Resources._009);
+            list.Add(Resources._010);
+            list.Add(Resources._100);
+            list.Add(Resources._101);
+            list.Add(Resources._102);
+            list.Add(Resources._103);
+            list.Add(Resources._104);
+            list.Add(Resources._105);
+            list.Add(Resources._106);
+            list.Add(Resources._107);
+            list.Add(Resources._108);
+            list.Add(Resources._109);
+            list.Add(Resources._110);
+        }
+
+
+        private void btnUP_Click(object sender, EventArgs e)
+        {
+            m++;
+            m_str = Convert.ToString(m);
+            MON.Text = m_str;
+        }
+
+        private void btnpump_h_Click(object sender, EventArgs e)
+        {
+            int randomNum = new Random().Next(9); //0-100
+            int index = randomNum + 11;//模擬隨機產生 一個值;
+            if (m >= m_h)
+            {
+                picResult.Image = list[index];
+                m = m - m_h;
+                m_rest();
+            }
+            else
+            {
+                MessageBox.Show("金額不足", "警告");
+            }
+        }
+
+        private void btnpump_l_Click(object sender, EventArgs e)
+        {
+            int randomNum = new Random().Next(5); //0-100
+            int index = randomNum + 1;//模擬隨機產生 一個值;
+            if (m >= m_l)
+            {
+                picResult.Image = list[index];
+                m = m - m_l;
+                m_rest();
+            }
+            else
+            {
+                MessageBox.Show("金額不足", "警告");
+            }
+        }
+
+
+
+        private void m_rest()
+        {
+            m_str = Convert.ToString(m);
+            MON.Text = m_str;
+            return;
+        }
+    }
+}
